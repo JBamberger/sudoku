30ad7a8320a6da412a91e5fdde72db40f792fec7
Jannik Bamberger <dev.jbamberger@gmail.com>
2020-10-27 17:31:44 +0100

    Change warp/unwarp implementation

--- src/app.cpp
+++ src/app.cpp
@@ -12,7 +12,7 @@ int
 main(int argc, char* argv[])
 {
 
-    cv::Mat sudokuImg = cv::imread(R"(../share/sudoku-sk.jpg)", 0);
+    cv::Mat sudokuImg = cv::imread(R"(../share/sudoku-sk.jpg)", IMREAD_GRAYSCALE );
     cv::resize(sudokuImg, sudokuImg, cv::Size(), 0.5, 0.5);
 
     DigitClassifier classifier = DigitClassifier();
@@ -27,18 +27,27 @@ main(int argc, char* argv[])
     cv::Mat resultImg;
     cv::cvtColor(sudokuImg, resultImg, COLOR_GRAY2BGR);
 
+    auto unwarp = [&sudoku](const cv::Point2d& p) {
+        cv::Mat warpedLocation = (cv::Mat_<double>(3, 1) << p.x, p.y, 1.f);
+        cv::Mat u = sudoku.unwarpMap * warpedLocation;
+        u = u / u.at<double>(2, 0);
+        return cv::Point2i(static_cast<int>(u.at<double>(0, 0)), static_cast<int>(u.at<double>(1, 0)));
+    };
+
     for (int i = 0; i < sudoku.cells.size(); i++) {
-        // cv::imshow("cell", sudoku.aligned(sudoku.cells.at(i)));
-        // cv::waitKey();
+        cv::Rect2i cell = sudoku.cells.at(i);
+
+        std::vector<cv::Point> pts = {
+            unwarp({ static_cast<double>(cell.x), static_cast<double>(cell.y) }),
+            unwarp({ static_cast<double>(cell.x + cell.width), static_cast<double>(cell.y) }),
+            unwarp({ static_cast<double>(cell.x + cell.width), static_cast<double>(cell.y + cell.height) }),
+            unwarp({ static_cast<double>(cell.x), static_cast<double>(cell.y + cell.height) })
+        };
+        cv::polylines(resultImg, pts, true, { 255, 0, 0 });
+
 
         if (digits.at(i) >= 0) {
-            cv::Rect2i cell = sudoku.cells.at(i);
-
-            cv::Mat warpedLocation =
-              (cv::Mat_<double>(3, 1) << (double)cell.x + cell.width * 0.1, (double)cell.y + cell.height * 0.9, 1.f);
-            cv::Mat u = sudoku.unwarpMap * warpedLocation;
-            u = u / u.at<double>(2, 0);
-            cv::Point2i location{ (int)u.at<double>(0, 0), (int)u.at<double>(1, 0) };
+            cv::Point2i location = unwarp({ (double)cell.x + cell.width * 0.1, (double)cell.y + cell.height * 0.9 });
 
             cv::putText(resultImg,
                         std::to_string(digits.at(i)),
@@ -49,15 +58,11 @@ main(int argc, char* argv[])
                         1,
                         cv::LINE_AA);
         }
-
-        // std::cout << digits.at(i) << std::endl;
     }
 
     Quad c = sudoku.corners;
-    cv::line(resultImg, c.tl, c.tr, { 0, 255, 0 });
-    cv::line(resultImg, c.tr, c.br, { 0, 255, 0 });
-    cv::line(resultImg, c.br, c.bl, { 0, 255, 0 });
-    cv::line(resultImg, c.bl, c.tl, { 0, 255, 0 });
+    std::vector<cv::Point> pts = { c.tl, c.tr, c.br, c.bl };
+    cv::polylines(resultImg, pts, true, { 0, 255, 0 });
     cv::rectangle(resultImg, sudoku.bbox, { 0, 0, 255 });
     cv::imshow("Result", resultImg);
     cv::waitKey();
