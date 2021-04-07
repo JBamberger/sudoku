6c570fa667e8327c46f64ab3061ed15b2cc6e516
Jannik Bamberger <dev.jbamberger@gmail.com>
2021-04-07 23:18:57 +0200

    Update binarization code

--- sudokupy/unwarp_sudoku.py
+++ sudokupy/unwarp_sudoku.py
@@ -1,13 +1,14 @@
 import math
-import os
 import time
 
 import cv2 as cv
 import numpy as np
+from skimage.filters import threshold_sauvola
 
-from classifier import deskew, classify_digit
-from detection_utils import in_resize, detect_sudoku, unwarp_patch, pad_contour, p2p_dist
-from utils import read_ground_truth
+from classifier import Net
+from detection_utils import in_resize, detect_sudoku, unwarp_patch, pad_contour, p2p_dist, SudokuNotFoundException
+from sudoku_solver import solve_sudoku
+from utils import rotation_correction, read_ground_truth
 
 
 def show(img, name='Image', no_wait=False):
@@ -17,38 +18,38 @@ def show(img, name='Image', no_wait=False):
 
 
 gt_annoatations = read_ground_truth(np.os.path.abspath('ground_truth_new.csv'))
+digit_classifier = Net()
+digit_classifier.load()
 
 
-# sudoku_path = annot[0][0]
-# sudoku_path = annot[18][0]
-# sudoku_path = annot[28][0]
+def adaptiveThresh(image, lower, upper):
+    assert lower < upper
+
+    adapt = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 701, C=2)
+    show(adapt, name='Binarized', no_wait=True)
+
+    lowerimg = np.zeros_like(adapt)
+    lowerimg[image < lower] = 255
+    show(lowerimg, name='lower', no_wait=True)
+
+    upperimg = np.zeros_like(adapt)
+    upperimg[image > upper] = 255
+    show(upperimg, name='upperimg')
+
+    adapt[image < lower] = 255
+    adapt[image > upper] = 0
+
+    return adapt
+
+
+def thresh_savoula(image, window_size=15, k=0.2):
+    threshold = threshold_sauvola(image, window_size=window_size, k=k)
+    image = (image < threshold).astype(np.uint8) * 255
+    return image
+
 
 def extract_cells(image):
-    image = image.copy()
-
-    sudoku = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    # show(sudoku, name='Gray', no_wait=True)
-
-    # erosion_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
-    # sudoku = cv.erode(sudoku, erosion_kernel)
-    # show(sudoku, name='Eroded', no_wait=True)
-
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize=(21, 21))
-    closing = cv.morphologyEx(sudoku, cv.MORPH_CLOSE, kernel)
-    # show(closing, name='Closing', no_wait=True)
-
-    sudoku = (sudoku / closing * 255).astype(np.uint8)
-    # show(sudoku, name='GrayNorm', no_wait=True)
-
-    threshold, sudoku = cv.threshold(sudoku, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-    # show(sudoku, name='Binarized', no_wait=True)
-
-    sudoku = cv.medianBlur(sudoku, 3)
-    # show(sudoku, name='Binarized+Median', no_wait=True)
-
-    dilation_kernel = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))
-    sudoku = cv.dilate(sudoku, dilation_kernel)
-    # show(sudoku, name='Dilated', no_wait=True)
+    sudoku = binarize_sudoku(image)
 
     cells = []
     contours, hierarchy = cv.findContours(sudoku, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
@@ -153,7 +154,7 @@ def extract_cells(image):
                 col_node = row_node
         # TODO: Detect failures here, i.e. when row_node didn't change
 
-    print(cell_to_node)
+    # print(cell_to_node)
 
     show(contour_canvas, name='Contours', no_wait=True)
 
@@ -181,7 +182,7 @@ def extract_cells(image):
         coordinates[:2, :] = lower
         coordinates[2:, :] = upper
 
-        coordinates = coordinates[[1,2,3,0]]
+        coordinates = coordinates[[1, 2, 3, 0]]
 
         padded_cell_patch = unwarp_patch(image, coordinates, out_size=patch_size)
 
@@ -191,89 +192,160 @@ def extract_cells(image):
     return cell_patches, cell_coords
 
 
-for file_path, coords in gt_annoatations:
+def binarize_sudoku(image):
+    sudoku = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # show(sudoku, name='Gray', no_wait=True)
+
+    # erosion_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+    # sudoku = cv.erode(sudoku, erosion_kernel)
+    # show(sudoku, name='Eroded', no_wait=True)
+
+    # hist, bins = np.histogram(sudoku, bins=np.arange(0, 255))
+    # plt.plot(hist)
+    # plt.axvline(x=255-threshold)
+    # plt.show()
+
+    sudoku = cv.GaussianBlur(sudoku, (5, 5), 0)
+    # show(sudoku, name='Blurred', no_wait=True)
+
+    thresh = 'sav'
+    if thresh == 'adaptive':
+        sudoku = cv.adaptiveThreshold(sudoku, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 51, C=2)
+    elif thresh == 'sav':
+        sudoku = thresh_savoula(sudoku, window_size=51)
+    else:
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize=(21, 21))
+        closing = cv.morphologyEx(sudoku, cv.MORPH_CLOSE, kernel)
+        # show(closing, name='Closing', no_wait=True)
+
+        sudoku = (sudoku / closing * 255).astype(np.uint8)
+        # show(sudoku, name='GrayNorm', no_wait=True)
+
+        threshold, _ = cv.threshold(sudoku, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+
+        # blur = cv.GaussianBlur(sudoku, (5, 5), 0)
+        # _, sudoku2 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+        # show(sudoku2, name='BinarizedGaussian', no_wait=True)
+
+        # threshold, sudoku = cv.threshold(sudoku, 127, 255, cv.THRESH_BINARY_INV)
+        # sudoku = cv.adaptiveThreshold(sudoku, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 701, C=2)
+        # sudoku = adaptiveThresh(sudoku, 50, 180)
+        # threshold, _ = cv.threshold(sudoku, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+        # threshold, sudoku = cv.threshold(sudoku, threshold+20, 255, cv.THRESH_BINARY_INV)
+    # print(f'Otsu`s thresh {threshold}')
+    # show(sudoku, name='Binarized', no_wait=True)
+
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize=(5, 5))
+    sudoku = cv.dilate(sudoku, kernel)
+    # show(sudoku, name='Binarized+dilated', no_wait=True)
+
+    sudoku = cv.medianBlur(sudoku, 7)
+    # show(sudoku, name='Binarized+Median', no_wait=True)
+
+    # dilation_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
+    # sudoku = cv.dilate(sudoku, dilation_kernel)
+    # show(sudoku, name='Dilated', no_wait=True)
+
+    # cv.waitKey()
+
+    return sudoku
+
+
+for sudoku_index, (file_path, gt_coords) in enumerate(gt_annoatations):
+    if sudoku_index < 0:
+        continue
+
     start = time.time()
     sudoku_img_org = cv.imread(file_path)
 
+    # Ensure that the sudoku is always rotated by at most 45 deg in either direction.
+    sudoku_img_org, gt_coords = rotation_correction(sudoku_img_org, gt_coords)
+
     # Scaling such that the longer side is 1024 px long
     input_downscale, sudoku_img = in_resize(sudoku_img_org)
+    image_center = (int(round(sudoku_img.shape[1] / 2)), int(round(sudoku_img.shape[0] / 2)))
 
+    # Visualization canvas
     canvas = sudoku_img.copy()
 
-    image_center = (int(round(sudoku_img.shape[1] / 2)), int(round(sudoku_img.shape[0] / 2)))
-    try:
-        pred_location = detect_sudoku(sudoku_img)
+    # Draw gt rect
+    gt_coords = np.array(gt_coords).reshape(4, 2)
+    gt_coords = (gt_coords * input_downscale).astype(np.int32)
+    cv.polylines(canvas, [gt_coords], True, (0, 255, 0), thickness=3)
 
-        # uw = coarse_unwarp(sudoku_img_org, pred_location / input_downscale)
-        # show(uw, name='coarse_unwarp')
-
-        # add padding to compensate for bounding boxes that fit too tight
-        padded_location = pad_contour(sudoku_img, pred_location)
-
-        # TODO: replace with actual orientation detection
-        padded_location = np.roll(padded_location, shift=2, axis=0)
-
-        # unwarp from original image for best warp quality
-        sudoku_coarse_unwarp, crop_grid = unwarp_patch(sudoku_img_org, padded_location / input_downscale,
-                                                       return_grid=True)
-        crop_grid *= input_downscale
-
-        cell_images, cell_coords = extract_cells(sudoku_coarse_unwarp)
-        cell_images = cell_images.astype(np.uint8)
-
-        out = np.zeros((81,), dtype=np.int32)
-        for i in range(81):
-            cell_patch = cell_images[i, :, :, :]
-            gray_cell = cv.cvtColor(cell_patch, cv.COLOR_BGR2GRAY)
-            # deskewed_cell = deskew(gray_cell)
-
-            pad = 6
-            _, pt = cv.threshold(gray_cell[pad:-pad, pad:-pad], 100, 255, cv.THRESH_BINARY_INV)
-            if np.count_nonzero(pt) > 50:
-                out[i] = classify_digit(gray_cell)
-            else:
-                out[i] = -1
-
-            # _, pt = cv.threshold(gray_cell, 100, 255, cv.THRESH_BINARY_INV)
-            # nnz = np.count_nonzero(pt)
-            #
-            # cell_path = os.path.join('extracted_digits',
-            #                          f'{nnz:08d}_{os.path.splitext(os.path.basename(file_path))[0]}_{i}.jpg')
-            # cv.imwrite(cell_path, cell_patch)
-
-            cell_center = cell_coords[i, :, :].mean(0).astype(np.int32)
-            cell_center = crop_grid[0, cell_center[1].item(), cell_center[0].item(), :].astype(np.int32)
-            cell_center = (cell_center[0].item(), cell_center[1].item())
-
-            # translate from cropping
-
-            cv.putText(canvas, str(out[i]), cell_center, cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=2)
-            # cv.drawMarker(canvas, cell_center, (0, 255, 0))
-
-        out = out.reshape((9, 9))
-        print(out)
-
-        # show(sudoku_coarse_unwarp, name='coarse_unwarp')
-
-        # cv.rectangle(canvas, p1, p2, (255, 0, 0), thickness=3)
-        cv.polylines(canvas, [pred_location], True, (255, 0, 0), thickness=3)
-
-        for i in range(4):
-            cv.putText(canvas, 'ABCD'[i], tuple(pred_location[i, :]),
-                       cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
-
-
-
-    except RuntimeError:
-        cv.putText(canvas, 'FAILED TO DETECT', image_center, cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=3)
-
+    # draw image center
     cv.drawMarker(canvas, image_center, (0, 0, 255))
 
-    coords = np.array(coords).reshape(4, 2)
-    coords = (coords * input_downscale).astype(np.int32)
-    cv.polylines(canvas, [coords], True, (0, 255, 0), thickness=3)
+    try:
+        pred_location = detect_sudoku(sudoku_img)
+    except SudokuNotFoundException as e:
+        print(f'Failed to detect sudoku. {e}')
 
-    print(time.time() - start)
+        cv.putText(canvas, 'Detection failed!', image_center, cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=3)
+        show(canvas, name='Bounds')
+        continue
+
+    # add padding to compensate for bounding boxes that fit too tight
+    padded_location = pad_contour(sudoku_img, pred_location)
+
+    # TODO: replace with actual orientation detection
+    padded_location = np.roll(padded_location, shift=2, axis=0)
+
+    # unwarp from original image for best warp quality
+    scaled_locations = padded_location / input_downscale
+    sudoku_coarse_unwarp, crop_grid = unwarp_patch(sudoku_img_org, scaled_locations, return_grid=True)
+    crop_grid *= input_downscale
+
+    cell_images, cell_coords = extract_cells(sudoku_coarse_unwarp)
+    cell_images = cell_images.astype(np.uint8)
+
+    out = np.zeros((81,), dtype=np.int32)
+    for i in range(81):
+        cell_patch = cell_images[i, :, :, :]
+        gray_cell = cv.cvtColor(cell_patch, cv.COLOR_BGR2GRAY)
+        # deskewed_cell = deskew(gray_cell)
+
+        pad = 6
+        _, pt = cv.threshold(gray_cell[pad:-pad, pad:-pad], 100, 255, cv.THRESH_BINARY_INV)
+        # print(f'{np.count_nonzero(pt): 4d} {np.var(gray_cell): 5.02f}')
+        if np.count_nonzero(pt) > 100 and np.var(gray_cell) > 500:
+            # out[i] = classify_digit(gray_cell)
+            out[i] = digit_classifier.classify(gray_cell)
+        else:
+            out[i] = 0
+
+    out = out.reshape((9, 9))
+    # print(out)
+
+    # _, pt = cv.threshold(gray_cell, 100, 255, cv.THRESH_BINARY_INV)
+    # nnz = np.count_nonzero(pt)
+    #
+    # cell_path = os.path.join('extracted_digits',
+    #                          f'{nnz:08d}_{os.path.splitext(os.path.basename(file_path))[0]}_{i}.jpg')
+    # cv.imwrite(cell_path, cell_patch)
+
+    solved_sudoku = solve_sudoku(out.tolist())
+    solved_sudoku = out if solved_sudoku is None else np.array([int(x) for x in solved_sudoku]).reshape(9, 9)
+    # print(solved_sudoku)
+
+    solved_sudoku = solved_sudoku.flatten()
+    for i in range(81):
+        cell_center = cell_coords[i, :, :].mean(0).astype(np.int32)
+        cell_center = crop_grid[0, cell_center[1].item(), cell_center[0].item(), :].astype(np.int32)
+        cell_center = (cell_center[0].item(), cell_center[1].item())
+
+        cv.putText(canvas, str(solved_sudoku[i]), cell_center, cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=2)
+        # cv.drawMarker(canvas, cell_center, (0, 255, 0))
+
+    cv.polylines(canvas, [pred_location], True, (255, 0, 0), thickness=3)
+    cv.line(canvas, tuple(pred_location[0, :]), tuple(pred_location[1, :]), (255, 0, 255), thickness=10)
+    cv.line(canvas, tuple(pred_location[1, :]), tuple(pred_location[2, :]), (255, 255, 0), thickness=10)
+
+    for i in range(4):
+        cv.putText(canvas, 'ABCD'[i], tuple(pred_location[i, :]),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+
+    print('Took ', time.time() - start)
     show(canvas, name='Bounds')
 
 # sudoku_brightness_normalized
